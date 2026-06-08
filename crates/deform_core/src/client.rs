0c33800013d2c81336966909968589fab948fd2f
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2026-06-08 15:07:34 +0100

    restructure and cleanup deps

---
+++ crates/deform_core/src/client.rs
@@ -0,0 +1,90 @@
+use std::{
+    collections::{HashMap, HashSet},
+    sync::{atomic::AtomicBool, Arc, Mutex, MutexGuard},
+};
+
+use tokio::sync::{mpsc, Notify};
+
+use crate::{
+    lobby::LobbyStatus, DeformError, DeformGameState, DeformResult, DeformUserLogic, Pubkey,
+    TickInfo,
+};
+
+/// A [`DeformClient`] acts as the frontend interface where the game interacts with the library, abstracting the underlying backend implementation.
+/// Currently, the client is completely agnostic to the backend.
+pub struct DeformClient<T: DeformUserLogic> {
+    /// Used to tell the backend to terminate
+    pub terminate: Arc<Notify>,
+    /// Channel used to set inputs
+    // FIX: in the future, this should be changed to no longer be a channel; instead client can access the inputs directly, like I do for reading state. When that happens I thing Inputs no longer needs to be Send
+    pub set_inputs_sender: mpsc::UnboundedSender<T::Inputs>,
+    /// Game state to be read by the client
+    pub sdk_game_state: Arc<Mutex<DeformReadState<T>>>,
+    /// Set to true by the backend thread when it exits (cleanly or due to error).
+    pub backend_dead: Arc<AtomicBool>,
+}
+
+/// The state that is returned by the SDK to your application.
+#[derive(serde::Serialize)]
+pub struct DeformReadState<T: DeformUserLogic> {
+    pub tick_info: TickInfo<T>,
+    /// The last known status the server has sent us
+    pub remote_status: LobbyStatus,
+    pub stats: Stats,
+    /// Your own data, so you can read it back when reading the rest of the state.
+    ///
+    /// NOTE: I had a lot of trouble deciding how to do this. The backends need mutable access, so I always have to use a mutex of some sort.
+    /// However, running the callbacks inside the lock is bad as I don't know how long the operations being done by the user are taking.
+    /// So, the approach I have chosen is to keep an owned T in the backend. After operations are done and the [`DeformReadState`] needs to be updated, it is cloned into here.
+    pub user_logic: T,
+    pub internal_error: DeformResult<()>,
+}
+
+impl<T: DeformUserLogic> DeformReadState<T> {
+    /// Create a new state when players are known
+    pub fn new(players: &HashSet<Pubkey>) -> Self {
+        let game_state = T::GameState::new(players);
+        let mut inputs = HashMap::new();
+        for player in players.iter() {
+            inputs.insert(player.clone(), T::Inputs::default());
+        }
+        let tick_info = TickInfo { game_state, inputs };
+
+        Self {
+            tick_info,
+            remote_status: Default::default(),
+            stats: Default::default(),
+            user_logic: Default::default(),
+            internal_error: Ok(()),
+        }
+    }
+}
+
+#[derive(serde::Serialize, Default, Clone)]
+pub struct Stats {
+    pub ping_ms: f64,
+}
+
+impl<T: DeformUserLogic> DeformClient<T> {
+    /// Returns the latest state along with other useful information.
+    /// To prevent unecessary cloning (and having to derive Clone), this is just a very thin
+    /// wrapper of locking the mutex. You must drop it as soon as possible to avoid contention.
+    pub fn read_state(&self) -> DeformResult<MutexGuard<'_, DeformReadState<T>>> {
+        let state = self
+            .sdk_game_state
+            .lock()
+            .map_err(|_| DeformError::LockPoisoned)?;
+
+        Ok(state)
+    }
+
+    pub fn set_inputs(&self, inputs: T::Inputs) -> DeformResult {
+        self.set_inputs_sender
+            .send(inputs)
+            .map_err(|_| DeformError::ChannelClosed)
+    }
+
+    pub fn shutdown(&self) {
+        self.terminate.notify_one()
+    }
+}
--- crates/deform_core/src/lib.rs
+++ crates/deform_core/src/lib.rs
@@ -1,18 +1,16 @@
-use std::{
-    collections::{HashMap, HashSet},
-    sync::{atomic::AtomicBool, Arc, Mutex, MutexGuard},
-};
+use std::collections::{HashMap, HashSet};
 use wincode::{config::DefaultConfig, SchemaRead, SchemaWrite};
-
-use tokio::sync::{mpsc, Notify};
-
-use crate::lobby::LobbyStatus;
 
 pub use deform_derive::Smooth;
 
+#[cfg(feature = "client")]
+pub mod client;
 pub mod error;
 pub mod lobby;
 pub mod smooth;
+
+#[cfg(feature = "client")]
+pub use client::{DeformClient, DeformReadState, Stats};
 
 pub use error::{DeformError, DeformResult};
 pub use smooth::{NoopSmoother, Smooth, SmoothParams, Smoothable, SmoothableField};
@@ -94,20 +92,6 @@ pub trait DeformUserLogic: Clone + Default + Send + 'static {
     }
 }
 
-/// A [`DeformClient`] acts as the frontend interface where the game interacts with the library, abstracting the underlying backend implementation.
-/// Currently, the client is completely agnostic to the backend.
-pub struct DeformClient<T: DeformUserLogic> {
-    /// Used to tell the backend to terminate
-    pub terminate: Arc<Notify>,
-    /// Channel used to set inputs
-    // FIX: in the future, this should be changed to no longer be a channel; instead client can access the inputs directly, like I do for reading state. When that happens I thing Inputs no longer needs to be Send
-    pub set_inputs_sender: mpsc::UnboundedSender<T::Inputs>,
-    /// Game state to be read by the client
-    pub sdk_game_state: Arc<Mutex<DeformReadState<T>>>,
-    /// Set to true by the backend thread when it exits (cleanly or due to error).
-    pub backend_dead: Arc<AtomicBool>,
-}
-
 #[derive(serde::Serialize, Clone)]
 pub struct TickInfo<T: DeformUserLogic> {
     /// The current game state at this tick
@@ -119,71 +103,6 @@ pub struct TickInfo<T: DeformUserLogic> {
     /// The inputs that, combined with the previous state, have led to this new state.
     // TODO: in sbf, this should use pinocchio pubkey
     pub inputs: HashMap<Pubkey, T::Inputs>,
-}
-
-/// The state that is returned by the SDK to your application.
-#[derive(serde::Serialize)]
-pub struct DeformReadState<T: DeformUserLogic> {
-    pub tick_info: TickInfo<T>,
-    /// The last known status the server has sent us
-    pub remote_status: LobbyStatus,
-    pub stats: Stats,
-    /// Your own data, so you can read it back when reading the rest of the state.
-    ///
-    /// NOTE: I had a lot of trouble deciding how to do this. The backends need mutable access, so I always have to use a mutex of some sort.
-    /// However, running the callbacks inside the lock is bad as I don't know how long the operations being done by the user are taking.
-    /// So, the approach I have chosen is to keep an owned T in the backend. After operations are done and the [`DeformReadState`] needs to be updated, it is cloned into here.
-    pub user_logic: T,
-    pub internal_error: DeformResult<()>,
-}
-
-impl<T: DeformUserLogic> DeformReadState<T> {
-    /// Create a new state when players are known
-    pub fn new(players: &HashSet<Pubkey>) -> Self {
-        let game_state = T::GameState::new(players);
-        let mut inputs = HashMap::new();
-        for player in players.iter() {
-            inputs.insert(player.clone(), T::Inputs::default());
-        }
-        let tick_info = TickInfo { game_state, inputs };
-
-        Self {
-            tick_info,
-            remote_status: Default::default(),
-            stats: Default::default(),
-            user_logic: Default::default(),
-            internal_error: Ok(()),
-        }
-    }
-}
-
-#[derive(serde::Serialize, Default, Clone)]
-pub struct Stats {
-    pub ping_ms: f64,
-}
-
-impl<T: DeformUserLogic> DeformClient<T> {
-    /// Returns the latest state along with other useful information.
-    /// To prevent unecessary cloning (and having to derive Clone), this is just a very thin
-    /// wrapper of locking the mutex. You must drop it as soon as possible to avoid contention.
-    pub fn read_state(&self) -> DeformResult<MutexGuard<'_, DeformReadState<T>>> {
-        let state = self
-            .sdk_game_state
-            .lock()
-            .map_err(|_| DeformError::LockPoisoned)?;
-
-        Ok(state)
-    }
-
-    pub fn set_inputs(&self, inputs: T::Inputs) -> DeformResult {
-        self.set_inputs_sender
-            .send(inputs)
-            .map_err(|_| DeformError::ChannelClosed)
-    }
-
-    pub fn shutdown(&self) {
-        self.terminate.notify_one()
-    }
 }
 
 pub trait DeformInputs:
